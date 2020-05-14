18527ed57b15649e59a3bd91420343a920df5f02
NoahLE <Noah@NoahLE.com>
2020-05-14 01:28:41 -0500

    Working

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,29 +1,60 @@
-const path = require("path")
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "../dist")
-        
+  mode: "development",
+  entry: "./src/index",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.m?js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [ path.resolve(__dirname, "src")]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".json", ".css"],
-        modules: ["node_modules", path.resolve(__dirname, "src")],
-    },
-    performance: {
-        hints: false
-    },
-    devtool: "source-map",
-    target: "web",
-    stats: "errors-only"
-}
+    extensions: [".js", ".json", ".css"],
+    modules: ["node_modules", path.resolve(__dirname, "src")],
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+  target: "web",
+  stats: "errors-only",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "ROFL",
+      template: "src/index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// import Component from "./Component";
+
+// Vue Router
+const routes = [{ path: "/", component: { template: "<div>hi</div>" } }];
+const router = new VueRouter({ routes });
+Vue.use(VueRouter);
+
+// App
+const app = new Vue({ router }).$mount("#app");
